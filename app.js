24b6550d386c35f3891f310ecaf20a9d4872d45d
Matthew Lin <darthpwner@yaho.com>
2015-04-04 02:59:14 -0700

    Added the plugins and rigged hand libraries

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var map = new google.maps.Map(document.getElementById('map-canvas'),
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-
-
 // Store frame for motion functions
 var previousFrame = null;
 var paused = false;
@@ -224,3 +221,57 @@ function pauseForGestures() {
     pauseOnGesture = false;
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+/*Swipe Gestures*/
+/*
+Leap.loop(function(frame) {
+
+  frame.hands.forEach(function(hand, index) {
+    var cat = ( cats[index] || (cats[index] = new Cat()) );
+    cat.setTransform(hand.screenPosition(), hand.roll());
+  });
+
+}).use('screenPosition', {scale: 0.25});
+*/
+
+var cats = {};
+
+Leap.loop(function(frame) {
+
+  frame.hands.forEach(function(hand, index) {
+
+    var cat = ( cats[index] || (cats[index] = new Cat()) );
+    cat.setTransform(hand.screenPosition(), hand.roll());
+
+  });
+
+}).use('screenPosition', {scale: 0.25});
+
+
+var Cat = function() {
+  var cat = this;
+  var img = document.createElement('img');
+  img.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/109794/cat_2.png';
+  img.style.position = 'absolute';
+  img.onload = function () {
+    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
+    document.body.appendChild(img);
+  }
+
+  cat.setTransform = function(position, rotation) {
+
+    img.style.left = position[0] - img.width  / 2 + 'px';
+    img.style.top  = position[1] - img.height / 2 + 'px';
+
+    img.style.transform = 'rotate(' + -rotation + 'rad)';
+
+    img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
+    img.style.OTransform = img.style.transform;
+
+  };
+
+};
+
+cats[0] = new Cat();
+//////////////////////////////////////////////////////////////////////////////
